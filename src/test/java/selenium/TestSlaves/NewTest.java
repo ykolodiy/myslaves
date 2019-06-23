e4373c1a79fcfa833bcc4ceec06d56745e0fb533
unknown <yuriy.kolodiy@gmail.com>
2019-06-22 19:42:17 -0500

    first commit 3

--- src/test/java/selenium/TestSlaves/NewTest.java
+++ src/test/java/selenium/TestSlaves/NewTest.java
@@ -1,9 +1,7 @@
 package selenium.TestSlaves;
-
-import org.openqa.selenium.By;		
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 	
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;	
 import org.testng.Assert;		
 import org.testng.annotations.Test;	
 import org.testng.annotations.BeforeTest;	
